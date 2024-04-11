6d982b9bddc7a7462c6a948b0fb8f006a6ed2c50
Åsne Haavengen <aasneha@stud.ntnu.com>
2024-04-11 11:20:31 +0200

    added header component

---
+++ assets/src/todos/pages/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+//import { FaSkiing } from "react-icons/fa";
+import "./headerstyles.css"
+
+
+export default function Header() {
+    return (
+        <header className="container">
+            <nav className="nav">
+                <a href="/" className="logo" style={{ fontSize: '40px' }}>Veien til sporet</a>
+                <ul>
+                    <li>
+                        <a href="/routs">Reiseplanlegger</a>
+                    </li>
+                    <li>
+                        <a href="/favorittes">Favoritter</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
